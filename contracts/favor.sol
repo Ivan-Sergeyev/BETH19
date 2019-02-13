68d29f781cbe65f2a3a85b6e63d8929dbc9117c4
Ivan Sergeev <i.i.sergeyev@gmail.com>
2019-02-13 15:32:56 +0100

    updated solidity contract (still in the works)

--- contracts/favor.sol
+++ contracts/favor.sol
@@ -1,122 +1,103 @@
 pragma solidity ^0.5.1;
 
-contract Favor {
-
-	struct BasicInfo {
-		bytes32 name;
-		bytes32 location;
-		bytes32 description;
-		uint category;
-	}
-
-	// note: this will be publicly visible on the whole blockchain! we don't want that!
-	struct ExtendedInfo {
-		bytes32 name;
-		bytes32 location;
-		bytes32 description;
-		uint category;
-	}
-
-	uint prev_favor_idx;
-	uint next_favor_idx;
-
-	BasicInfo basic_info;
-	ExtendedInfo extended_info;
-
-	address requester;
-	address performer;
-	uint cost;
-
-	function getBasicInfo(address addr) external view returns(bytes32,bytes32,bytes32,uint) {
-		return ;
-	}
-
-	function getExtendedInfo(address addr) external view returns(ExtendedInfo) {
-		require(addr == requester || addr == performer);
-		return ExtendedInfo;
-	}
-
-	function 
-
-}
-
-
 contract Master {
-	
-	struct User {
-		uint balance;
-		mapping(uint => uint) favors_requesting;
-		mapping(uint => uint) favors_performing;
-		bool is_new;
-		uint performed_count;
-		uint rep;
-	}
 
-	mapping(address => User) public users;
-	uint user_count;
-
-	mapping(uing =>Favor) favors;
-	uint head_favor;
-
-	uint fvr_count;
-
-}
-
-
-contract LinkedList {
-
-  event AddEntry(bytes32 head,uint number,bytes32 name,bytes32 next);
-
-  uint public length = 0;//also used as nonce
-
-  struct Object{
-    bytes32 next;
-    uint number;
-    bytes32 name;
-  }
-
-  bytes32 public head;
-  mapping (bytes32 => Object) public objects;
-
-  function LinkedList(){}
-
-  function addEntry(uint _number,bytes32 _name) public returns (bool){
-    Object memory object = Object(head,_number,_name);
-    bytes32 id = sha3(object.number,object.name,now,length);
-    objects[id] = object;
-    head = id;
-    length = length+1;
-    AddEntry(head,object.number,object.name,object.next);
-  }
-
-  //needed for external contract access to struct
-  function getEntry(bytes32 _id) public returns (bytes32,uint,bytes32){
-    return (objects[_id].next,objects[_id].number,objects[_id].name);
-  }
-
-
-  //------------------ totalling stuff to explore list mechanics 
-
-  function total() public constant returns (uint) {
-    bytes32 current = head;
-    uint totalCount = 0;
-    while( current != 0 ){
-      totalCount = totalCount + objects[current].number;
-      current = objects[current].next;
+    // note: this will be publicly visible on the whole blockchain! are we OK with that?
+    struct FavorMetadata {
+        string name;
+        string location;
+        string description;
+        uint category;
     }
-    return totalCount;
-  }
 
-  function setTotal() public returns (bool) {
-    writtenTotal = total();
-    return true;
-  }
+    struct Favor {
+        uint next_favors_list_idx;  // link to next favor in list
 
-  function resetTotal() public returns (bool) {
-    writtenTotal = 0;
-    return true;
-  }
+        address requester;
+        address performer;
+        uint cost_fvr;
 
-  uint public writtenTotal;
+        FavorMetadata basic_info;
+        FavorMetadata extended_info;  // todo: should be hidden from public and only available to requester and performer!
+    }
+
+    struct User {
+        bool is_new = true;
+        uint performed_count;
+
+        uint balance;  // fvr
+        uint rep;
+
+        mapping(uint => uint) active_favors;  // linked list
+        uint active_favors_head = 0;
+    }
+
+    mapping(address => User) public users;
+    uint user_count = 0;
+
+    uint fvr_count = 0;
+
+    mapping(uint => Favor) favors_list;  // linked list
+    uint favors_list_head = 0;
+
+    function isFavorEmpty(Favor memory _favor) internal returns(bool) {
+        ;
+    }
+
+    function isUserFavorEmpty(address _user, uint _favor_idx) internal returns(bool) {
+        Favor memory favor = users[_user].active_favors[_favor_idx];
+        return (favor.requester == 0x0) && (favor.performer == 0x0);
+    }
+
+    function nextFavorsListHead() internal {
+        while(!isFavorEmpty(,active_favors_head) != 0x0) {
+            ++active_favors_head;
+        }
+    }
+
+    function isFavorEmpty(uint _favor_idx) internal returns (bool) {
+        Favor memory favor = favors_list[_favor_idx];
+        return (favor.requester == 0x0) && (favor.performer == 0x0);
+    }
+
+    function isFavorActive(uint _favor_idx) internal returns (bool) {
+        return (favors_list[_favor_idx].requester != 0x0) &&
+               (favors_list[_favor_idx].performer != 0x0);
+    }
+
+    function nextFavorsListHead() internal {
+        while(!isFavorEmpty(favors_list_head) != 0x0) {
+            ++favors_list_head;
+        }
+    }
+
+    function newFavor(address _requester, uint _cost_fvr,
+            string _basic_name, string _basic_location, string _basic_description, uint _basic_category,
+            string _extended_name, string _extended_location, string _extended_description, uint _extended_category) public returns(uint) {
+        // create new favor
+        FavorMetadata memory basic_info = FavorMetadata(_basic_name,_basic_location,_basic_description,_basic_category);
+        FavorMetadata memory extended_info = FavorMetadata(_extended_name,_extended_location,_extended_description,_extended_category);
+        Favor memory new_favor = Favor(favors_list_head,_requester,0x0,_cost_fvr,basic_info,extended_info);
+
+        // add favor to user's list of active favors
+        // todo
+
+        // add favor to list
+        uint cur_head = favors_list_head;
+        favors_list[favors_list_head] = new_favor;
+        nextFavorsListHead();
+        return cur_head;
+    }
+
+    function getBasicInfo(address addr) external view returns(string,string,string,uint) {
+        return (basic_info.name,basic_info.location,basic_info.description,basic_info.category);
+    }
+
+    function getExtendedInfo(address addr) external view returns(string,string,string,uint) {
+        require(addr == requester || addr == performer);
+        return (extended_info.name,extended_info.location,extended_info.description,extended_info.category);
+    }
+
+
 
 }
