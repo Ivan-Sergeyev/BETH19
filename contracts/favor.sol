03c090dd073a827a5bf18d05232366e4a35f0506
Ivan Sergeev <i.i.sergeyev@gmail.com>
2019-02-13 16:20:59 +0100

    updated solidity contract (still work in progress)

--- contracts/favor.sol
+++ contracts/favor.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.5.1;
 
 contract Master {
 
+    event FavorComplete();  // todo
+
     // note: this will be publicly visible on the whole blockchain! are we OK with that?
     struct FavorMetadata {
         string name;
@@ -11,8 +13,6 @@ contract Master {
     }
 
     struct Favor {
-        uint next_favors_list_idx;  // link to next favor in list
-
         address requester;
         address performer;
         uint cost_fvr;
@@ -27,74 +27,52 @@ contract Master {
 
         uint balance;  // fvr
         uint rep;
-
-        mapping(uint => uint) active_favors;  // linked list
-        uint active_favors_head = 0;
     }
 
     mapping(address => User) public users;
-    uint user_count = 0;
 
-    uint fvr_count = 0;
+    uint constant MAX_NUM_FAVORS = 10;
+    Favor[MAX_NUM_FAVORS] favors;
 
-    mapping(uint => Favor) favors_list;  // linked list
-    uint favors_list_head = 0;
-
-    function isFavorEmpty(Favor memory _favor) internal returns(bool) {
-        ;
+    // favor states
+    function _isFavorEmpty(uint _favor_idx) private returns(bool) {
+        return (favors[_favor_idx].requester == 0x0) && (favors[_favor_idx].performer == 0x0);
     }
 
-    function isUserFavorEmpty(address _user, uint _favor_idx) internal returns(bool) {
-        Favor memory favor = users[_user].active_favors[_favor_idx];
-        return (favor.requester == 0x0) && (favor.performer == 0x0);
+    function _isFavorPending(uint _favor_idx) private returns(bool) {
+        return (favors[_favor_idx].requester != 0x0) ^ (favors[_favor_idx].performer != 0x0);
     }
 
-    function nextFavorsListHead() internal {
-        while(!isFavorEmpty(,active_favors_head) != 0x0) {
-            ++active_favors_head;
-        }
-    }
-
-    function isFavorEmpty(uint _favor_idx) internal returns (bool) {
-        Favor memory favor = favors_list[_favor_idx];
-        return (favor.requester == 0x0) && (favor.performer == 0x0);
-    }
-
-    function isFavorActive(uint _favor_idx) internal returns (bool) {
-        return (favors_list[_favor_idx].requester != 0x0) &&
-               (favors_list[_favor_idx].performer != 0x0);
-    }
-
-    function nextFavorsListHead() internal {
-        while(!isFavorEmpty(favors_list_head) != 0x0) {
-            ++favors_list_head;
-        }
+    function _isFavorActive(uint _favor_idx) private returns(bool) {
+        return (favors[_favor_idx].requester != 0x0) && (favors[_favor_idx].performer != 0x0);
     }
 
     function newFavor(address _requester, uint _cost_fvr,
             string _basic_name, string _basic_location, string _basic_description, uint _basic_category,
-            string _extended_name, string _extended_location, string _extended_description, uint _extended_category) public returns(uint) {
-        // create new favor
-        FavorMetadata memory basic_info = FavorMetadata(_basic_name,_basic_location,_basic_description,_basic_category);
-        FavorMetadata memory extended_info = FavorMetadata(_extended_name,_extended_location,_extended_description,_extended_category);
-        Favor memory new_favor = Favor(favors_list_head,_requester,0x0,_cost_fvr,basic_info,extended_info);
-
-        // add favor to user's list of active favors
-        // todo
-
-        // add favor to list
-        uint cur_head = favors_list_head;
-        favors_list[favors_list_head] = new_favor;
-        nextFavorsListHead();
-        return cur_head;
+            string _extended_name, string _extended_location, string _extended_description, uint _extended_category) external returns(uint) {
+        for(int i = 0; i < MAX_NUM_FAVORS; ++i) {
+            if(_isFavorEmpty(i)) {
+                favors[i] = Favor(favors_list_head,_requester,0x0,_cost_fvr,
+                                  FavorMetadata(_basic_name,_basic_location,_basic_description,_basic_category),
+                                  FavorMetadata(_extended_name,_extended_location,_extended_description,_extended_category));
+                return i;
+            }
+        }
+        // failed to add new favor: all spots taken
+        return MAX_NUM_FAVORS;
     }
 
-    function getBasicInfo(address addr) external view returns(string,string,string,uint) {
-        return (basic_info.name,basic_info.location,basic_info.description,basic_info.category);
+    function getNumFavors() external view returns(uint) {
+        return MAX_NUM_FAVORS;
     }
 
-    function getExtendedInfo(address addr) external view returns(string,string,string,uint) {
-        require(addr == requester || addr == performer);
+    function getFavorBasicInfo(uint _favor_idx) external view returns(address,address,uint,string,string,string,uint) {
+        return (favors[_favor_idx].requester,favors[_favor_idx].performer,favors[_favor_idx].cost_fvr,
+                favors[_favor_idx].basic_info.name,favors[_favor_idx].basic_info.location,favors[_favor_idx].basic_info.description,favors[_favor_idx].basic_info.category);
+    }
+
+    function getExtendedInfo(address _addr) external view returns(string,string,string,uint) {
+        require(_addr == requester || _addr == performer, "Access denied");
         return (extended_info.name,extended_info.location,extended_info.description,extended_info.category);
     }
 
