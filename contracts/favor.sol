be4ad45dd11ebd36ec7d40f7710bf417580fe2e9
Ivan Sergeev <i.i.sergeyev@gmail.com>
2019-02-13 18:51:07 +0100

    basic contract functionality

--- contracts/favor.sol
+++ contracts/favor.sol
@@ -1,21 +1,22 @@
 pragma solidity ^0.5.1;
 
-contract Master {
-
-    event FavorCreated();   // todo
-    event FavorAccepted();  // todo
-    event FavorComplete();  // todo
-    event FavorAborted();   // todo
+contract Favor {
+    event ListingCreated(uint _listing_idx, address _user_address);
+    event ListingAccepted(uint _listing_idx, address _user_address);
+    event ListingAbortRequest(uint _listing_idx, address _user_address);
+    event ListingAborted(uint _listing_idx);
+    event ListingCompleteRequest(uint _listing_idx, address _user_address);
+    event ListingCompleted(uint _listing_idx);
 
     // note: this will be publicly visible on the whole blockchain! are we OK with that?
-    struct FavorMetadata {
-        string name;
-        string location;
-        string description;
+    struct ListingMetadata {
+        bytes32 name;
+        bytes32 location;
+        bytes32 description;
         uint category;
     }
 
-    struct Favor {
+    struct Listing {
         address requester;
         address performer;
         uint cost_fvr;
@@ -26,8 +27,8 @@ contract Master {
         bool abort_requester;
         bool abort_performer;
 
-        FavorMetadata basic_info;
-        FavorMetadata extended_info;  // todo: this should be hidden from public and only available to requester and performer!
+        ListingMetadata basic_info;
+        ListingMetadata extended_info;  // todo: this should be hidden from public and only available to requester and performer!
     }
 
     struct User {
@@ -40,167 +41,171 @@ contract Master {
     }
 
     uint constant MAX_NUM_FAVORS = 10;
-    Favor[MAX_NUM_FAVORS] favors;
+    Listing[MAX_NUM_FAVORS] listings;
 
     mapping(address => User) public users;
 
-    // states of favors
-    function _isFavorEmpty(uint _favor_idx) private returns(bool) {
-        return (favors[_favor_idx].requester == 0x0) && (favors[_favor_idx].performer == 0x0);
+    // get user balance
+    function getUserBalance(address _user_addr) external view returns(uint) {
+        return users[_user_addr].balance_fvr;
     }
 
-    function _isFavorRequestPending(uint _favor_idx) private returns(bool) {
-        return (favors[_favor_idx].requester != 0x0) && (favors[_favor_idx].performer == 0x0);
+    // states of listings
+    function isListingEmpty(uint _listing_idx) public view returns(bool) {
+        return (listings[_listing_idx].requester == address(0)) && (listings[_listing_idx].performer == address(0));
     }
 
-    function _isFavorOfferPending(uint _favor_idx) private returns(bool) {
-        return (favors[_favor_idx].requester == 0x0) && (favors[_favor_idx].performer != 0x0);
+    function _isListingRequestPending(uint _listing_idx) private view returns(bool) {
+        return (listings[_listing_idx].requester != address(0)) && (listings[_listing_idx].performer == address(0));
     }
 
-    function _isFavorActive(uint _favor_idx) private returns(bool) {
-        return (favors[_favor_idx].requester != 0x0) && (favors[_favor_idx].performer != 0x0);
+    function _isListingOfferPending(uint _listing_idx) private view returns(bool) {
+        return (listings[_listing_idx].requester == address(0)) && (listings[_listing_idx].performer != address(0));
+    }
+
+    function _isListingActive(uint _listing_idx) private view returns(bool) {
+        return (listings[_listing_idx].requester != address(0)) && (listings[_listing_idx].performer != address(0));
     }
 
     // check if user is requester or performer
-    function _isUserInvolved(address _user_addres, uint _favor_idx) private returns(bool) {
-        return (_user_address == favors[_favor_idx].requester) || (_user_addres == favors[_favor_idx].performer);
+    function _isUserInvolved(address _user_address, uint _listing_idx) private view returns(bool) {
+        return (_user_address == listings[_listing_idx].requester) || (_user_address == listings[_listing_idx].performer);
     }
 
-    // get basic information about favors
-    function getNumFavors() external view returns(uint) {
+    // get basic information about listings
+    function getNumListings() external pure returns(uint) {
         return MAX_NUM_FAVORS;
     }
 
-    function getFavorBasicInfo(uint _favor_idx) external view returns(address,address,uint,string,string,string,uint) {
-        return (favors[_favor_idx].requester,favors[_favor_idx].performer,favors[_favor_idx].cost_fvr,
-                favors[_favor_idx].basic_info.name,favors[_favor_idx].basic_info.location,favors[_favor_idx].basic_info.description,favors[_favor_idx].basic_info.category);
+    function getListingBasicInfo(uint _listing_idx) external view returns(address,address,uint,bytes32,bytes32,bytes32,uint) {
+        return (listings[_listing_idx].requester,listings[_listing_idx].performer,listings[_listing_idx].cost_fvr,
+                listings[_listing_idx].basic_info.name,listings[_listing_idx].basic_info.location,listings[_listing_idx].basic_info.description,listings[_listing_idx].basic_info.category);
     }
 
-    // create favors
-    function _createFavor(uint _favor_idx, address _requester_addr, address _performer_addr, uint _cost_fvr,
-            string _basic_name, string _basic_location, string _basic_description, uint _basic_category,
-            string _extended_name, string _extended_location, string _extended_description, uint _extended_category) private {
-        favors[_favor_idx].requester = _requester_addr;
-        favors[_favor_idx].performer = _performer_addr;
-        favors[_favor_idx].cost_fvr = _cost_fvr;
+    // create listings
+    function _createListing(uint _listing_idx, address _requester_addr, address _performer_addr, uint _cost_fvr,
+            bytes32 _basic_name, bytes32 _basic_location, bytes32 _basic_description, uint _basic_category,
+            bytes32 _extended_name, bytes32 _extended_location, bytes32 _extended_description, uint _extended_category) private {
+        listings[_listing_idx].requester = _requester_addr;
+        listings[_listing_idx].performer = _performer_addr;
+        listings[_listing_idx].cost_fvr = _cost_fvr;
 
-        favors[_favor_idx].complete_requester = false;
-        favors[_favor_idx].complete_performer = false;
+        listings[_listing_idx].complete_requester = false;
+        listings[_listing_idx].complete_performer = false;
 
-        favors[_favor_idx].abort_requester = false;
-        favors[_favor_idx].abort_performer = false;
+        listings[_listing_idx].abort_requester = false;
+        listings[_listing_idx].abort_performer = false;
 
-        favors[_favor_idx].basic_info = FavorMetadata(_basic_name,_basic_location,_basic_description,_basic_category);
-        favors[_favor_idx].extended_info = FavorMetadata(_extended_name,_extended_location,_extended_description,_extended_category);
+        listings[_listing_idx].basic_info = ListingMetadata(_basic_name,_basic_location,_basic_description,_basic_category);
+        listings[_listing_idx].extended_info = ListingMetadata(_extended_name,_extended_location,_extended_description,_extended_category);
     }
 
-    function createFavorRequest(address _requester_addr, uint _cost_fvr,
-            string _basic_name, string _basic_location, string _basic_description, uint _basic_category,
-            string _extended_name, string _extended_location, string _extended_description, uint _extended_category) external returns(uint) {
-        require (users[_requester_addr].balance_fvr >= 2 * _cost_fvr);
-        for(int i = 0; i < MAX_NUM_FAVORS; ++i) {
-            if(_isFavorEmpty(i)) {
-                users[_requester_addr].balance_fvr -= 2 * _cost_fvr;
-                _createFavor(i,_requester_addr,0x0,_cost_fvr,
-                             _basic_name,_basic_location,_basic_description,_basic_category,
-                             _extended_name,_extended_location,_extended_description,_extended_category);
-                return i;
-            }
-        }
-        // failed to add new favor: all spots taken
-        return MAX_NUM_FAVORS;
+    function createListingRequest(uint _listing_idx, address _requester_addr, uint _cost_fvr,
+            bytes32 _basic_name, bytes32 _basic_location, bytes32 _basic_description, uint _basic_category,
+            bytes32 _extended_name, bytes32 _extended_location, bytes32 _extended_description, uint _extended_category) external {
+        require(isListingEmpty(_listing_idx),"Not empty listing");
+        require(users[_requester_addr].balance_fvr >= 2 * _cost_fvr,"Insufficient funds");
+        users[_requester_addr].balance_fvr -= 2 * _cost_fvr;
+        _createListing(_listing_idx,_requester_addr,address(0),_cost_fvr,
+                     _basic_name,_basic_location,_basic_description,_basic_category,
+                     _extended_name,_extended_location,_extended_description,_extended_category);
+        emit ListingCreated(_listing_idx, _requester_addr);
     }
 
-    function createFavorOffer(address _performer_addr, uint _cost_fvr,
-            string _basic_name, string _basic_location, string _basic_description, uint _basic_category,
-            string _extended_name, string _extended_location, string _extended_description, uint _extended_category) external returns(uint) {
-        require(users[_performer_addr].balance_fvr >= _cost_fvr);
-        for(int i = 0; i < MAX_NUM_FAVORS; ++i) {
-            if(_isFavorEmpty(i)) {
-                users[_performer_addr].balance_fvr -= _cost_fvr;
-                favors[i] = Favor(favors_list_head,0x0,_performer_addr,_cost_fvr,
-                                  FavorMetadata(_basic_name,_basic_location,_basic_description,_basic_category),
-                                  FavorMetadata(_extended_name,_extended_location,_extended_description,_extended_category));
-                return i;
-            }
-        }
-        // failed to add new favor: all spots taken
-        return MAX_NUM_FAVORS;
-    }
-
-    // accept favors
-    function acceptFavorRequest(address _performer_addr, uint _favor_idx) external {
-        require(_isFavorRequestPending(_favor_idx));
-        require(users[_performer_addr].balance_fvr >= _cost_fvr);
+    function createListingOffer(uint _listing_idx, address _performer_addr, uint _cost_fvr,
+            bytes32 _basic_name, bytes32 _basic_location, bytes32 _basic_description, uint _basic_category,
+            bytes32 _extended_name, bytes32 _extended_location, bytes32 _extended_description, uint _extended_category) external {
+        require(isListingEmpty(_listing_idx),"Not empty listing");
+        require(users[_performer_addr].balance_fvr >= _cost_fvr,"Insufficient funds");
         users[_performer_addr].balance_fvr -= _cost_fvr;
-        favors[i].performer = _performer_addr;
+        _createListing(_listing_idx,address(0),_performer_addr,_cost_fvr,
+                     _basic_name,_basic_location,_basic_description,_basic_category,
+                     _extended_name,_extended_location,_extended_description,_extended_category);
+        emit ListingCreated(_listing_idx, _performer_addr);
     }
 
-    function acceptFavorOffer(address _requeser_addr, uint _favor_idx) external {
-        require(_isFavorRequestPending(_favor_idx));
-        require(users[_requeser_addr].balance_fvr >= 2 * _cost_fvr);
-        users[_requeser_addr].balance_fvr -= 2 * _cost_fvr;
-        favors[i].requester = _requeser_addr;
+    // accept listings
+    function acceptListingRequest(uint _listing_idx, address _performer_addr) external {
+        require(_isListingRequestPending(_listing_idx),"Not a pending listing request");
+        require(users[_performer_addr].balance_fvr >= listings[_listing_idx].cost_fvr,"Insufficient funds");
+        users[_performer_addr].balance_fvr -= listings[_listing_idx].cost_fvr;
+        listings[_listing_idx].performer = _performer_addr;
+        emit ListingAccepted(_listing_idx, _performer_addr);
+    }
+
+    function acceptListingOffer(uint _listing_idx, address _requeser_addr) external {
+        require(_isListingOfferPending(_listing_idx),"Not a pending listing offer");
+        require(users[_requeser_addr].balance_fvr >= 2 * listings[_listing_idx].cost_fvr,"Insufficient funds");
+        users[_requeser_addr].balance_fvr -= 2 * listings[_listing_idx].cost_fvr;
+        listings[_listing_idx].requester = _requeser_addr;
+        emit ListingAccepted(_listing_idx, _requeser_addr);
     }
 
     // // get detailed info
-    // function getExtendedInfo(address _addr) external view returns(string,string,string,uint) {
+    // function getExtendedInfo(address _addr) external view returns(bytes32,bytes32,bytes32,uint) {
     //     require(_addr == requester || _addr == performer, "Access denied");
     //     return (extended_info.name,extended_info.location,extended_info.description,extended_info.category);
     // }
 
-    // abort favor
-    function _wipeFavor(uint _favor_idx) private {
-        favors[_favor_idx].requester = 0x0;
-        favors[_favor_idx].performer = 0x0;
-        favors[_favor_idx].cost_fvr = 0;
+    // wipe listing
+    function _wipeListing(uint _listing_idx) private {
+        listings[_listing_idx].requester = address(0);
+        listings[_listing_idx].performer = address(0);
+        listings[_listing_idx].cost_fvr = 0;
 
-        favors[_favor_idx].complete_requester = false;
-        favors[_favor_idx].complete_performer = false;
+        listings[_listing_idx].complete_requester = false;
+        listings[_listing_idx].complete_performer = false;
 
-        favors[_favor_idx].abort_requester = false;
-        favors[_favor_idx].abort_performer = false;
+        listings[_listing_idx].abort_requester = false;
+        listings[_listing_idx].abort_performer = false;
 
-        favors[_favor_idx].basic_info = FavorMetadata("","","",0);
-        favors[_favor_idx].extended_info = FavorMetadata("","","",0);
+        listings[_listing_idx].basic_info = ListingMetadata("","","",0);
+        listings[_listing_idx].extended_info = ListingMetadata("","","",0);
     }
 
-    function requestAbortFavor(address _user_addres, uint _favor_idx) external {
-        require(_isFavorActive(_favor_idx));
-        require(_isUserInvolved(_user_addres, _favor_idx));
+    // request for abort
+    function requestAbortListing(uint _listing_idx, address _user_addres) external {
+        require(_isListingActive(_listing_idx),"Not an active listing");
+        require(_isUserInvolved(_user_addres, _listing_idx),"Access denied");
 
-        if(_user_addres == favors[_favor_idx].requester) {
-            favors[_favor_idx].abort_requester = true;
+        // record request
+        if(_user_addres == listings[_listing_idx].requester) {
+            listings[_listing_idx].abort_requester = true;
         } else {
-            favors[_favor_idx].abort_performer = true;
+            listings[_listing_idx].abort_performer = true;
         }
+        emit ListingAbortRequest(_listing_idx, _user_addres);
 
-        if(favors[_favor_idx].abort_requester && favors[_favor_idx].abort_performer) {
+        if(listings[_listing_idx].abort_requester && listings[_listing_idx].abort_performer) {
             // revert fvr transaction
-            users[favors[_favor_idx].requester].balance_fvr += 2 * favors[_favor_idx].cost_fvr;
-            users[favors[_favor_idx].performer].balance_fvr += favors[_favor_idx].cost_fvr;
+            users[listings[_listing_idx].requester].balance_fvr += 2 * listings[_listing_idx].cost_fvr;
+            users[listings[_listing_idx].performer].balance_fvr += listings[_listing_idx].cost_fvr;
             // clean up
-            _wipeFavor(_favor_idx);
+            _wipeListing(_listing_idx);
+            emit ListingAborted(_listing_idx);
         }
     }
 
-    // complete favor
-    function requestCompleteFavor(address _user_addres, uint _favor_idx) external {
-        require(_isFavorActive(_favor_idx));
-        require(_isUserInvolved(_user_addres, _favor_idx));
+    // request for completion
+    function requestCompleteListing(uint _listing_idx, address _user_addres) external {
+        require(_isListingActive(_listing_idx),"Not an active listing");
+        require(_isUserInvolved(_user_addres, _listing_idx),"Access denied");
 
-        if(_user_addres == favors[_favor_idx].requester) {
-            favors[_favor_idx].complete_requester = true;
+        // record request
+        if(_user_addres == listings[_listing_idx].requester) {
+            listings[_listing_idx].complete_requester = true;
         } else {
-            favors[_favor_idx].complete_performer = true;
+            listings[_listing_idx].complete_performer = true;
         }
+        emit ListingCompleteRequest(_listing_idx, _user_addres);
 
-        if(favors[_favor_idx].complete_requester && favors[_favor_idx].complete_performer) {
+        if(listings[_listing_idx].complete_requester && listings[_listing_idx].complete_performer) {
             // finalize transaction
-            users[favors[_favor_idx].requester].balance_fvr += favors[_favor_idx].cost_fvr;
-            users[favors[_favor_idx].performer].balance_fvr += 2 * favors[_favor_idx].cost_fvr;
+            users[listings[_listing_idx].requester].balance_fvr += listings[_listing_idx].cost_fvr;
+            users[listings[_listing_idx].performer].balance_fvr += 2 * listings[_listing_idx].cost_fvr;
             // clean up
-            _wipeFavor(_favor_idx);
+            _wipeListing(_listing_idx);
+            emit ListingCompleted(_listing_idx);
         }
     }
 }
