9463bbed7b3812625e798cb0198ea629270e1b1a
Ivan Sergeev <i.i.sergeyev@gmail.com>
2019-02-14 00:20:09 +0100

    replaced broken altered example files for pet-shop with working original versions (aka clean slate)

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,20 +1,19 @@
 pragma solidity ^0.5.0;
 
 contract Adoption {
-  address[16] public adopters;
+    address[16] public adopters;
 
-  // Adopting a pet
-  function adopt(uint petId) public returns (uint) {
-    require(petId >= 0 && petId <= 15);
+    // Adopting a pet
+    function adopt(uint petId) public returns (uint) {
+      require(petId >= 0 && petId <= 15);
 
-    adopters[petId] = msg.sender;
+      adopters[petId] = msg.sender;
 
-    return petId;
-  }
+      return petId;
+    }
 
-  // Retrieving the adopters
-  function getAdopters() public view returns (address[16] memory) {
-    return adopters;
-  }
-
+    // Retrieving the adopters
+    function getAdopters() public view returns (address[16] memory) {
+      return adopters;
+    }
 }
