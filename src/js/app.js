48940435b504b8004e6d414bf5eccf6fd81e3f74
Semion Rozov <rozovs@ethz.ch>
2019-02-14 09:17:34 +0100

    added bootstrap-table

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,70 @@
 App = {
   web3Provider: null,
   contracts: {},
+<<<<<<< HEAD
+  account: '0x0',
+  hasVoted: false,
+
+  // init: function() {
+  //   return App.initWeb3();
+  // },
+  //
+  // initWeb3: function() {
+  //   // TODO: refactor conditional
+  //   if (typeof web3 !== 'undefined') {
+  //     // If a web3 instance is already provided by Meta Mask.
+  //     App.web3Provider = web3.currentProvider;
+  //     web3 = new Web3(web3.currentProvider);
+  //   } else {
+  //     // Specify default instance if no web3 instance provided
+  //     App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+  //     web3 = new Web3(App.web3Provider);
+  //   }
+  //   return App.initContract();
+  // },
+  //
+  // initContract: function() {
+  //   $.getJSON("Favor.json", function(Favor) {
+  //     // Instantiate a new truffle contract from the artifact
+  //     App.contracts.Favor = TruffleContract(Favor);
+  //     // Connect provider to interact with contract
+  //     App.contracts.Favor.setProvider(App.web3Provider);
+  //
+  //     App.listenForEvents();
+  //
+  //     return App.render();
+  //   });
+  // },
+  //
+  // // Listen for events emitted from the contract
+  // listenForEvents: function() {
+  //   App.contracts.Favor.deployed().then(function(instance) {
+  //     // Restart Chrome if you are unable to receive this event
+  //     // This is a known issue with Metamask
+  //     // https://github.com/MetaMask/metamask-extension/issues/2393
+  //
+  //   }).then(function() {
+  //       console.log("pass render")
+  //       // Reload when a new vote is recorded
+  //       App.render();
+  //     });
+  // },
+  //
+  // render: function() {
+  //   var FavorInstance;
+  //   // Load account data
+  //   web3.eth.getAccounts(function(err, account) {
+  //     if (err === null) {
+  //       App.account = account[0];
+  //     }
+  //   });
+  //
+  //   // Load contract data
+  //   App.contracts.Favor.deployed().then(function(instance) {
+  //     FavorInstance = instance;
+  //   });
+  // },
+=======
 
   init: async function() {
     // Load pets.
@@ -112,11 +176,37 @@ App = {
       });
     });
   }
+>>>>>>> 3a2ee3e3c868df510bf1ff0db5e97344eb994797
 
 };
 
 $(function() {
   $(window).load(function() {
-    App.init();
+    // App.init();
   });
 });
+
+window.operateEvents = {
+    'click .like': function (e, value, row) {
+      alert('You click like action, row: ' + JSON.stringify(row))
+    },
+    'click .remove': function (e, value, row) {
+      alert('You click remove action, row: ' + JSON.stringify(row))
+    }
+  }
+
+  function operateFormatter(value, row, index) {
+    return [
+      '<div class="pull-left">',
+      '<a href="https://github.com/wenzhixin/' + value + '" target="_blank">' + value + '</a>',
+      '</div>',
+      '<div class="pull-right">',
+      '<a class="like" href="javascript:void(0)" title="Like">',
+      '<i class="glyphicon glyphicon-heart"></i>',
+      '</a>  ',
+      '<a class="remove" href="javascript:void(0)" title="Remove">',
+      '<i class="glyphicon glyphicon-remove"></i>',
+      '</a>',
+      '</div>'
+    ].join('')
+  }
